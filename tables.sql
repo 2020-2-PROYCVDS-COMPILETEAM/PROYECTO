abace5510f2aa4bf557b41f9a8357d5e445e36bf
joel carvajal <joel.carvajal@mail.escuelaing.edu.co>
2020-10-30 23:00:16 -0500

    desarrollo inicial - 3

---
+++ tables.sql
@@ -0,0 +1,6 @@
+-- -----------------------------------------------------
+-- Table `VI_TIPOITEM`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `EQUIPOS` (
+  `id` INT(11) NOT NULL,
+  `descripcion` VARCHAR(50)  NOT NULL);
