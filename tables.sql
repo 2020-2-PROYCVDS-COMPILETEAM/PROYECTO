ea61e36f27e9caf6d40859999665c27fbaeb3fcb
joel carvajal <joel.carvajal@mail.escuelaing.edu.co>
2020-11-03 01:49:20 -0500

    diagrama de clases

--- tables.sql
+++ tables.sql
@@ -1,44 +1,100 @@
--- -----------------------------------------------------
--- Table `VI_TIPOITEM`
--- -----------------------------------------------------
-create table equipo(
-    id serial primary key,
-	laboratorioID varchar(10),
-	activo boolean not null,
-	nombre varchar(20) not null);
+/* tablas "elementos*/
+
+create table Laboratorio (
+	id int,
+	nombre varchar,
+	descripcion varchar,
+	activo boolean,
+	PRIMARY KEY (id));
+
+create table Equipo (
+	id int,
+	nombre varchar,
+	descripcion varchar,
+	laboratorio int,
+	activo boolean,
+	PRIMARY KEY (id),
+	FOREIGN KEY (laboratorio) REFERENCES Laboratorio(id));
+	
+create table Elemento (
+	id int,
+	nombre varchar,
+	descripcion varchar,
+	marca varchar,
+	Equipo int,
+	PRIMARY KEY (id),
+	FOREIGN KEY (Equipo) REFERENCES Equipo(id));
+
+create table usuario (
+	correo varchar,
+	contraseña varchar,
+	username varchar,
+	nombre varchar,
+	rol varchar,
+	PRIMARY KEY (correo));
+	
+/* tablas "reporte"*/
+
+create table Reporte (
+	id int, 
+	descripcion varchar,
+	usuario varchar,
+	PRIMARY KEY (id),
+	FOREIGN KEY (usuario) REFERENCES usuario(correo));
+
+create table Novedad (
+	id int, 
+	descripcion varchar,
+	usuario varchar,
+	PRIMARY KEY (id),
+	FOREIGN KEY (usuario) REFERENCES usuario(correo));
+
+/* tablas uniones Reportes*/
+
+create table Reporte_Equipo (
+	id_Reporte int, 
+	id_Equipo int,
+	CONSTRAINT PK_Reporte_Equipo PRIMARY KEY (id_Reporte,id_Equipo),
+	FOREIGN KEY (id_Reporte) REFERENCES Reporte(id),
+	FOREIGN KEY (id_Equipo) REFERENCES Equipo(id));
+
+create table Reporte_Laboratorio (
+	id_Reporte int, 
+	id_Laboratorio int,
+	CONSTRAINT PK_Reporte_Laboratorio PRIMARY KEY (id_Reporte,id_Laboratorio),
+	FOREIGN KEY (id_Reporte) REFERENCES Reporte(id),
+	FOREIGN KEY (id_Laboratorio) REFERENCES laboratorio(id));
+
+create table Reporte_Elemento (
+	id_Reporte int, 
+	id_Elemento int,
+	CONSTRAINT PK_Reporte_Elemento PRIMARY KEY (id_Reporte,id_Elemento),
+	FOREIGN KEY (id_Reporte) REFERENCES Reporte(id),
+	FOREIGN KEY (id_Elemento) REFERENCES elemento(id));
+
+/* tablas uniones Novedades*/
+
+create table Novedad_Equipo (
+	id_Novedad int, 
+	id_Equipo int,
+	CONSTRAINT PK_Novedad_Equipo PRIMARY KEY (id_Novedad,id_Equipo),
+	FOREIGN KEY (id_Novedad) REFERENCES novedad(id),
+	FOREIGN KEY (id_Equipo) REFERENCES Equipo(id));
+
+create table Novedad_Laboratorio (
+	id_Novedad int, 
+	id_Laboratorio int,
+	CONSTRAINT PK_Novedad_Laboratorio PRIMARY KEY (id_Novedad,id_Laboratorio),
+	FOREIGN KEY (id_Novedad) REFERENCES novedad(id),
+	FOREIGN KEY (id_Laboratorio) REFERENCES laboratorio(id));
+
+create table Novedad_Elemento (
+	id_Novedad int, 
+	id_Elemento int,
+	CONSTRAINT PK_Novedad_Elemento PRIMARY KEY (id_Novedad,id_Elemento),
+	FOREIGN KEY (id_Novedad) REFERENCES novedad(id),
+	FOREIGN KEY (id_Elemento) REFERENCES elemento(id));
 
 
 
-CREATE TABLE IF NO EXISTS 'ELEMENTO'(
-    id int(11) NOT NULL,
-    tipo VARCHAR (50) NOT NULL,
-    equipo VARCHAR (50) NOT NULL,
-    marca VARCHAR (50) NOT NULL,
-    nombre VARCHAR (50) NOT NULL,
-    activo BOOLEAN (50) NOT NULL);
 
-
-CREATE TABLE IF NO EXISTS 'NOVEDAD'(
-    id int (10) NOT NOT NULL,
-	elementoID integer,
-	equipoID integer,
-	fecha DATE not null,
-	titulo varchar(200) not null,
-	usuarioID varchar(50) not null,
-	detalle varchar(500)not null,
-);
-
-create table usuario(
-    username varchar(50) primary key,
-	estado varchar(10) not null,
-	rol varchar(20) not null,
-	correo varchar(100) not null,
-	nombreCompleto varchar(100) not null ,
-	contrasena varchar(254) not null);
-
-create table laboratorio
-	(id varchar(10) primary key,
-	nombre varchar(30) not null,
-	descripcion varchar(500) not null,
-	capacidadequipos integer not null,
-	activo boolean not null);
