547daa70233eefa2e30afa36ccca7c36da9e69eb
Daniel Porras <Edward.porras@mail.escuelaing.edu.co>
2020-11-04 20:49:51 -0500

    correccion

--- src/main/java/edu/eci/cvds/samples/dao/mybatis/MyBATISElementoDAO.java
+++ src/main/java/edu/eci/cvds/samples/dao/mybatis/MyBATISElementoDAO.java
@@ -1,14 +1,15 @@
 package edu.eci.cvds.samples.dao.mybatis;
+import edu.eci.cvds.samples.dao.ElementoDAO;
 import edu.eci.cvds.samples.dao.mybatis.mappers.ElementoMapper;
 import com.google.inject.Inject;
 
-public class MyBATISElementoDAO implements ElementoMapper{
+public class MyBATISElementoDAO implements ElementoDAO {
 
     @Inject
     private ElementoMapper elementoMapper;
 
     @Override
-    public void registrarElemento(String tipo, String marca, String nombre) {
+    public void registrarElemento(String tipo, String marca, String nombre, int id) {
         elementoMapper.registrarElemento(tipo,marca,nombre);
     }
 }
