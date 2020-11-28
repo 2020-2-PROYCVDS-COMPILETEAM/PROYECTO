ef79573daacff556f4acf48b18f6b2a0786d3ad5
joel carvajal <joel.carvajal@mail.escuelaing.edu.co>
2020-11-28 00:17:20 -0500

    avance css

--- src/main/java/edu/eci/cvds/samples/entities/Novedad.java
+++ src/main/java/edu/eci/cvds/samples/entities/Novedad.java
@@ -1,9 +1,4 @@
 package edu.eci.cvds.samples.entities;
 
 public class Novedad {
-
-    private int id;
-    private
-
-
 }
--- tables.sql
+++ tables.sql
@@ -8,118 +8,88 @@ create table Laboratorio (
 	PRIMARY KEY (id));
 
 create table Equipo (
-	id serial primary key,
+	id int,
 	nombre varchar,
 	descripcion varchar,
 	laboratorio int,
 	activo boolean,
+	PRIMARY KEY (id),
 	FOREIGN KEY (laboratorio) REFERENCES Laboratorio(id));
 
-insert into Equipo(id,nombre,descripcion,laboratorio,activo) values (1,'dell','equipo del lab',null,true);
-	
 create table Elemento (
-	id serial primary key,
-	tipo varchar,
-	equipo int,
-	activo boolean,
-	marca varchar,
+	id int,
 	nombre varchar,
-	Foreign Key (equipo) references Equipo(id));
-
-insert into elemento(id,tipo,equipo,activo,marca,nombre) values (1,'pantalla',null,true,'dell','mk');
-
+	descripcion varchar,
+	marca varchar,
+	Equipo int,
+	PRIMARY KEY (id),
+	FOREIGN KEY (Equipo) REFERENCES Equipo(id));
 
 create table usuario (
 	correo varchar,
 	contraseña varchar,
 	username varchar,
 	nombre varchar,
-	rol varchar,
 	PRIMARY KEY (correo));
-	
-/* tablas "reporte"*/
 
-create table Reporte (
-	id int, 
-	descripcion varchar,
-	usuario varchar,
-	PRIMARY KEY (id),
-	FOREIGN KEY (usuario) REFERENCES usuario(correo));
-
-create table Novedad (
-	id int, 
-	descripcion varchar,
-	usuario varchar,
-	PRIMARY KEY (id),
-	FOREIGN KEY (usuario) REFERENCES usuario(correo));
-
-/* tablas uniones Reportes*/
-
-create table Reporte_Equipo (
-	id_Reporte int, 
-	id_Equipo int,
-	CONSTRAINT PK_Reporte_Equipo PRIMARY KEY (id_Reporte,id_Equipo),
-	FOREIGN KEY (id_Reporte) REFERENCES Reporte(id),
-	FOREIGN KEY (id_Equipo) REFERENCES Equipo(id));
-
-create table Reporte_Laboratorio (
-	id_Reporte int, 
-	id_Laboratorio int,
-	CONSTRAINT PK_Reporte_Laboratorio PRIMARY KEY (id_Reporte,id_Laboratorio),
-	FOREIGN KEY (id_Reporte) REFERENCES Reporte(id),
-	FOREIGN KEY (id_Laboratorio) REFERENCES laboratorio(id));
-
-create table Reporte_Elemento (
-	id_Reporte int, 
-	id_Elemento int,
-	CONSTRAINT PK_Reporte_Elemento PRIMARY KEY (id_Reporte,id_Elemento),
-	FOREIGN KEY (id_Reporte) REFERENCES Reporte(id),
-	FOREIGN KEY (id_Elemento) REFERENCES elemento(id));
-
-/* tablas uniones Novedades*/
+/* tablas "novedad*/
 
 create table Novedad_Equipo (
-	id_Novedad int, 
+	id int,
 	id_Equipo int,
-	CONSTRAINT PK_Novedad_Equipo PRIMARY KEY (id_Novedad,id_Equipo),
-	FOREIGN KEY (id_Novedad) REFERENCES novedad(id),
+	descripcion varchar,
+	usuario varchar,
+	PRIMARY KEY (id),
+	FOREIGN KEY (usuario) REFERENCES usuario(correo),
 	FOREIGN KEY (id_Equipo) REFERENCES Equipo(id));
 
-create table Novedad_Laboratorio (
-	id_Novedad int, 
-	id_Laboratorio int,
-	CONSTRAINT PK_Novedad_Laboratorio PRIMARY KEY (id_Novedad,id_Laboratorio),
-	FOREIGN KEY (id_Novedad) REFERENCES novedad(id),
-	FOREIGN KEY (id_Laboratorio) REFERENCES laboratorio(id));
-
 create table Novedad_Elemento (
-	id_Novedad int, 
+	id int,
 	id_Elemento int,
-	CONSTRAINT PK_Novedad_Elemento PRIMARY KEY (id_Novedad,id_Elemento),
-	FOREIGN KEY (id_Novedad) REFERENCES novedad(id),
+	descripcion varchar,
+	usuario varchar,
+	PRIMARY KEY (id),
+	FOREIGN KEY (usuario) REFERENCES usuario(correo),
 	FOREIGN KEY (id_Elemento) REFERENCES elemento(id));
 
+--laboratorio
+INSERT INTO laboratorio
+(id, nombre, descripcion, activo)
+VALUES(0, 'lab de prueba', 'este es una lab para probar', true);
 
 
+-- equipo
+INSERT INTO equipo
+(id, nombre, descripcion, laboratorio, activo)
+VALUES(0, 'equipo de prueba', 'este es un equipo de prueba', 0, true);
 
 
+-- elemento
+INSERT INTO elemento
+(id, nombre, descripcion, marca, equipo)
+VALUES(0, 'elemento prueba', 'este es un elemento de prueba', 'prueba', 0);
 
-drop table Novedad_Elemento;
-drop table Novedad_Equipo;
-drop table Novedad_Laboratorio;
+-- usuario
+INSERT INTO usuario
+(correo, contraseña, username, nombre)
+VALUES('prueba', 'prueba', 'prueba', 'prueba');
 
-drop table reporte_elemento;
-drop table reporte_equipo;
-drop table reporte_laboratorio;
+--novedad elemento
+INSERT INTO novedad_elemento
+(id, id_elemento, descripcion, usuario)
+VALUES(0, 0, 'una novedad de prueba', 'prueba');
 
-drop table Novedad;
-drop table reporte;
-drop table usuario;
-drop table elemento;
-drop table equipo;
-drop table laboratorio;
-
+--novedad equipo
+INSERT INTO novedad_equipo
+(id, id_equipo, descripcion, usuario)
+VALUES(0, 0, 'una novedad de prueba', 'prueba');
 
 
+drop  table elemento  ;
+drop  table equipo ;
+drop  table laboratorio ;
+drop table novedad_elemento ;
+drop table novedad_equipo ;
+drop table usuario ;
 
 
